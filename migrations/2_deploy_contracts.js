fab1a925a844ef94347a4888302161d75caa7fdd
tserg <tse.rong.gary@gmail.com>
2020-05-16 23:34:03 +0800

    docs: updated README
    README updated with local deployment instructions.
    .gitignore created for node_modules and build folders, and
    2_deploy_contracts.js.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 var FDT_ERC20Extension = artifacts.require("FDT_ERC20Extension");
 
 module.exports = function(deployer) {
-  deployer.deploy(FDT_ERC20Extension, "MSC", "Musicakes", "0x841D4197A1dB5966B1b18A9563c11d81E9f001E6");
+  deployer.deploy(FDT_ERC20Extension, "MSC", "Musicakes", "<<ADDRESS>>");
 
 };
